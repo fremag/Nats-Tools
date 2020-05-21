e8cbae978e802ea728fa4a35db26cd7cea00aa79
fremag <fremag.gh@gmail.com>
2020-05-21 12:38:56 +0200

    Add publish script to have a single exe file (win+linux)

--- publish.bat
+++ publish.bat
@@ -1,1 +1,2 @@
-dotnet publish -o publish -r win-x64 -c release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true nats-tools.csproj 
+dotnet publish -o publish-win -r win-x64 -c release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true nats-tools.csproj 
+dotnet publish -o publish-linux -r linux-x64 -c release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true nats-tools.csproj
